eb12929fed5e722a8fcfdb3a1b91c709000d5b51
Bohdan Zaiats <bohdanzaiats999@.gmail.com>
2018-10-04 17:03:29 +0300

    Created View,Model,ViewModel folders

---
+++ Snake/ViewModel/SnakeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Snake.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.ViewModel
+{
+    public class SnakeViewModel
+    {
+        ISnakeService snakeService;
+        public event PropertyChangedEventHandler PropertyChanged;
+        public SnakeViewModel(ISnakeService snakeService)
+        {
+            this.snakeService = snakeService;
+        }
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
